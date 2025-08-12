e54dfd8a3a15a40ff5da3a4a21452366a85d0b8f
MATHEUS BEZERRA <matheustto2@gmail.com>
2025-08-12 00:12:19 +0000

    Realizei o desenvolviemnto o programa completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,9 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    char estado;
+    char [30] código_da_carta;
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
